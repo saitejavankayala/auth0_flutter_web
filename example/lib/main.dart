7c691c45d0a927ec85418f4844c76be2a8fb2fc7
Saiteja Vankayala <saiteja.vankayala@tudip.com>
2024-04-22 17:54:19 +0530

    fixed minor bug

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,12 @@
-import 'package:auth0_web_flutter/auth0_flutter_web.dart';
 import 'package:flutter/material.dart';
+import 'package:auth0_web_flutter/auth0_flutter_web.dart';
 
 void main() {
+  // add the auth0domain
+  Auth0FlutterWeb.auth0Domain = "enter the auth0 domain";
+  //add the auth0ClientID
+  Auth0FlutterWeb.auth0ClientId = "enter the auth0 client id";
+  Auth0FlutterWeb.setPathUrlStrategy();
   runApp(const MyApp());
 }
 
